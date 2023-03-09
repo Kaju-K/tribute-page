730ca7fee63539af819e944130d77b5bad255200
Kaju-K <marcelokogut3@gmail.com>
2023-03-09 14:32:54 +0200

    making the menu close when choosing the section

--- js/menuButton.js
+++ js/menuButton.js
@@ -1,8 +1,20 @@
 menuButton = document.querySelector(".menu-button")
 menuImage = document.getElementById("menu-image")
 navigation = document.querySelector(".navigation")
+navigationLinks = document.querySelectorAll(".links")
+
+const windowWidth = window.innerWidth
 
 menuButton.addEventListener("click", () => {
     ["fa-bars", "fa-xmark"].map( img => menuImage.classList.toggle(img))
     navigation.classList.toggle("navigation--active")
 } )
+
+if (windowWidth <= 640) {
+    navigationLinks.forEach(link => {
+        link.addEventListener("click", event => {
+            ["fa-bars", "fa-xmark"].map( img => menuImage.classList.toggle(img))
+            navigation.classList.toggle("navigation--active")
+        })
+    });
+}
